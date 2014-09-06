d1d9128a358c1d6f3d020ed3d71106feef1ed26a
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2014-09-06 14:14:12 +0100

    Fixed a logic error with departments due to copypasta, and also added a DOB entry section.

--- CreateRecord.py
+++ CreateRecord.py
@@ -35,24 +35,41 @@ def CreateRecord():
                     NameComplete == True
                     break
         while DepartmentComplete == False:
-            name = enterbox(msg='Input Employee Department: ', title='Employee Tracker', default='', strip=True, image=None, root=None)
-            if isNone(name) == True:
+            department = enterbox(msg='Input Employee Department: ', title='Employee Tracker', default='', strip=True, image=None, root=None)
+            if isNone(department) == True:
                 msgbox(msg='Please enter a department.', title='Employee Tracker', ok_button='OK', image=None, root=None)
-            elif isInt(name) == True:
+            elif isInt(department) == True:
                 msgbox(msg='Please enter a valid department.', title='Employee Tracker', ok_button='OK', image=None, root=None)
             else:
-                nameCheck = enterbox(msg='Reenter Employee Department: ', title='Employee Tracker', default='', strip=True, image=None, root=None)
-                if isNone(name) == True:
+                departmentCheck = enterbox(msg='Reenter Employee Department: ', title='Employee Tracker', default='', strip=True, image=None, root=None)
+                if isNone(departmentCheck) == True:
                     msgbox(msg='Please enter a department.', title='Employee Tracker', ok_button='OK', image=None, root=None)
-                elif isInt(name) == True:
+                elif isInt(departmentCheck) == True:
                     msgbox(msg='Please enter a valid department.', title='Employee Tracker', ok_button='OK', image=None, root=None)
-                elif name != nameCheck:
+                elif department != departmentCheck:
                     msgbox(msg='The departments didnt match. Please try again.', title='Employee Tracker', ok_button='OK', image=None, root=None)
                 else:
                     DepartmentComplete == True
                     break
+        while DOBComplete == False:
+            DOB = enterbox(msg='Input Employee DOB (DD/MM/YY): ', title='Employee Tracker', default='', strip=True, image=None, root=None)
+            if isNone(DOB) == True:
+                msgbox(msg='Please enter a DOB.', title='Employee Tracker', ok_button='OK', image=None, root=None)
+            elif DOBCheck(DOB) == True:
+                msgbox(msg='Please enter a valid DOB.', title='Employee Tracker', ok_button='OK', image=None, root=None)
+            else:
+                DOBCheck = enterbox(msg='Reenter Employee DOB: ', title='Employee Tracker', default='', strip=True, image=None, root=None)
+                if isNone(DOBCheck) == True:
+                    msgbox(msg='Please enter a DOB.', title='Employee Tracker', ok_button='OK', image=None, root=None)
+                elif DOBCheck(DOBCheck) != True:
+                    msgbox(msg='Please enter a valid DOB.', title='Employee Tracker', ok_button='OK', image=None, root=None)
+                elif DOB != DOBCheck:
+                    msgbox(msg='The DOBs didnt match. Please try again.', title='Employee Tracker', ok_button='OK', image=None, root=None)
+                else:
+                    DOBComplete == True
+                    break
             
 
-#TODO : DOB, Gender, Salary
+#TODO : Gender, Salary
 #TODO : Unique Code
 #TODO : Update base files.
